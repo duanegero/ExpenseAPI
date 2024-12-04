6df0dc7c0340595d5985b371092d87f6aa22407e
duanegero <duanegero11@gmail.com>
2024-12-03 23:09:03 -0400

    adding comments

--- src/routes/support.js
+++ src/routes/support.js
@@ -8,7 +8,7 @@ const newUserRequest = [];
 
 //defining route for POST requests
 router.post("/", (req, res) => {
-  //creating details object to handle body of request
+  //creating variable to handle body of request
   const { details } = req.body;
 
   //if no deatils return error
@@ -29,17 +29,23 @@ router.post("/", (req, res) => {
   res.status(200).json({ message: "Support request recieved!" });
 });
 
+//defining route for POST requests
 router.post("/new-user", (req, res) => {
+  //creating variable to handle body of request
   const { newUserName, newUserRole } = req.body;
 
+  //if no name or role reture error
   if (!newUserName || !newUserRole) {
     return res.status(400).json({ error: "Name and role are required" });
   }
 
+  //pushing the request object to array
   newUserRequest.push({ newUserName, newUserRole, timestamp: new Date() });
 
+  //log new user info
   console.log("New User Request:", newUserName, newUserRole);
 
+  //respond with json
   res.status(200).json({ message: "New User request recieved!" });
 });
 
