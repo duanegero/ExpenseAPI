6fe2a672bfa79b975ca218b76b1dcfd9ae754ad6
duanegero <duanegero11@gmail.com>
2024-12-06 09:07:21 -0400

    adding comments to contact.js

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -2,17 +2,21 @@
 const express = require("express");
 const router = express.Router();
 
+//creating a array variable to handle requests made
 const callbackRequest = [];
 
 router.post("/", (req, res) => {
+  //variables handle details n request body
   const { name, phoneNumber, email } = req.body;
 
+  //if not enough details for request return error
   if (!name && (!phoneNumber || !email)) {
     return res
       .status(400)
       .json({ error: "Name and phone number or email required " });
   }
 
+  //pushing request to array
   callbackRequest.push({
     name,
     phoneNumber,
@@ -20,7 +24,9 @@ router.post("/", (req, res) => {
     timestapm: new Date(),
   });
 
+  //log request for testing
   console.log("Callback Request: ", { name, phoneNumber, email });
+  //return ok status with message
   res.status(200).json({ message: "Callback request recieved " });
 });
 
